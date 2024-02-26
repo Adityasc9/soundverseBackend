507782ad649e3b56b407af4f1355f5a7cdf03969
Adityasc9 <adityasc16@gmail.com>
2024-02-26 01:29:22 -0500

    e

--- models/Users.js
+++ models/Users.js
@@ -1,36 +1,16 @@
 const mongoose = require("mongoose");
 
-const stripeCheckoutSessionSchema = new Schema({
-  id: String,
-  object: String,
-  after_expiration: { type: Schema.Types.Mixed, default: null },
-  allow_promotion_codes: { type: Schema.Types.Mixed, default: null },
-  amount_subtotal: Number,
-  amount_total: Number,
-  automatic_tax: { type: Schema.Types.Mixed, default: {} }, // Assuming an object structure, adjust as necessary
-  billing_address_collection: { type: Schema.Types.Mixed, default: null },
-  cancel_url: String,
-  client_reference_id: { type: Schema.Types.Mixed, default: null },
-  client_secret: String,
-  consent: { type: Schema.Types.Mixed, default: null },
-  consent_collection: { type: Schema.Types.Mixed, default: null },
-  created: Number,
-  currency: String,
-  currency_conversion: { type: Schema.Types.Mixed, default: null },
-  // Add other fields as necessary...
-  customer: String,
-  customer_email: String,
-  expires_at: Number,
-  invoice: String,
-  line_items: { type: Schema.Types.Mixed, default: {} }, // Assuming an object structure, adjust as necessary
-  livemode: Boolean,
-  mode: String,
-  payment_status: String,
-  subscription: String,
-  success_url: String,
-  total_details: { type: Schema.Types.Mixed, default: {} } // Assuming an object structure, adjust as necessary
-}, { _id: false }); // Prevents Mongoose from creating an _id for this subdocument
-
+const stripeCheckoutSessionSchema = new Schema(
+  {
+    id: String, // Unique identifier for the session
+    amount_total: Number, // Total amount charged
+    currency: String, // Currency of the charge
+    customer: String, // Customer ID
+    payment_status: String, // Status of the payment
+    // You can still access other fields not defined here when querying the documents
+  },
+  { _id: false, strict: false }
+); 
 
 const userSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId },
